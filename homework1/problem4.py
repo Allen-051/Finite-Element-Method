c09a09e5c8334e457e20fd17250a4b4affb72602
邱昱倫11372009 <0501yulunallen@gmail.com>
2025-03-01 17:58:31 +0800

    Update problem4.py

--- homework1/problem4.py
+++ homework1/problem4.py
@@ -1,5 +1,20 @@
 # problem 4.1 寫出一個break程式並說明其流程及結果
-
+def guess_name(names):
+    correct_name = '芒果'
+    try:
+        names = input('請猜Allen最愛吃的水果：')
+        if names == correct_name:
+            print('恭喜你答對了!\n')
+            return True
+        else:
+            raise ValueError('歐歐...答錯了。小提示：是夏天才有的水果。再猜一次吧!')
+        
+    except ValueError as ve:
+            print(ve)
+            return False
+while True:
+     if guess_name('芒果') == True:
+          break
 
 #  problem 4.2 用if取出1到100中不是2、3、5、7倍數的數字
 result = []
